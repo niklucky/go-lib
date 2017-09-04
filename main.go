35cb7d3ae8d48b9b45bf7640aa86ac544337df52
Nikita <iam@niklucky.com>
2017-09-04 15:25:10 +0300

    (feat) Added reflection helper

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"io/ioutil"
 	"path/filepath"
+	"reflect"
+	"time"
 )
 
 func ReadFile(filename string) ([]byte, error) {
@@ -12,4 +14,30 @@ func ReadFile(filename string) ([]byte, error) {
 	}
 
 	return ioutil.ReadFile(file)
+}
+
+func GetStructTags(item interface{}, tagName string, strict interface{}) []string {
+	isStrict := true
+	if strict != nil {
+		isStrict = strict.(bool)
+	}
+	var tags []string
+	st := reflect.TypeOf(item)
+	for i := 0; i < st.NumField(); i++ {
+		field := st.Field(i)
+		fieldTag := field.Tag.Get(tagName)
+		if fieldTag != "" {
+			tags = append(tags, fieldTag)
+		} else if isStrict == false {
+			tags = append(tags, field.Name)
+		}
+	}
+	return tags
+}
+
+func GetISOTimemillisecons() string {
+	return time.Now().Format("2006-01-02T15:03:04.999")
+}
+func GetISOTimeSeconds() string {
+	return time.Now().Format("2006-01-02T15:03:04")
 }
